5d83d549505ce6c359d7b079fa57903c677c6b5c
DuncanLevings <levings636>
2021-03-04 13:53:56 -0500

    user profile img and navigation

--- app/src/main/java/sheridan/sharmupm/vegit_capstone/MainActivity.kt
+++ app/src/main/java/sheridan/sharmupm/vegit_capstone/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_classifyproducts, R.id.navigation_groceryList, R.id.navigation_notifications, R.id.navigation_diet))
+                R.id.navigation_home, R.id.navigation_classifyproducts, R.id.navigation_groceryList, R.id.navigation_diet, R.id.userProfile))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
