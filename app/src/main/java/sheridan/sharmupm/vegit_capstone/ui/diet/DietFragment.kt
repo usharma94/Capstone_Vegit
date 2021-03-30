e298a6f01dd3f46dd00e4467166c4230a858b1d7
Upma <Upma.Sharma@ontario.ca>
2021-03-30 13:56:19 -0400

    Fixed location of progress bar in login page, changed color scheme and added in the remember me check box.

--- app/src/main/java/sheridan/sharmupm/vegit_capstone/ui/diet/DietFragment.kt
+++ app/src/main/java/sheridan/sharmupm/vegit_capstone/ui/diet/DietFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import sheridan.sharmupm.vegit_capstone.R
@@ -107,3 +108,40 @@ class DietFragment : Fragment() {
         }
     }
 }
+
+
+//Testing out original method using view
+//class DietFragment : Fragment() {
+//
+//    private lateinit var dietList: List<DietModel>
+//
+//    override fun onCreateView(
+//            inflater: LayoutInflater, container: ViewGroup?,
+//            savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//        val view: View = inflater.inflate(R.layout.diet_cardview, container, false)
+//        val vegDes: String = getString(R.string.vegetarian_desc)
+//        val veganDes: String = getString(R.string.vegan_desc)
+//        val imgVegetarian =  R.drawable.vegetarian
+//        val imgVegan =  R.drawable.vegan
+//        val imgCustom =  R.drawable.custom
+//        dietList = listOf(
+//            DietModel(false, imgVegetarian, "Vegetarian", vegDes),
+//            DietModel(false, imgVegan, "Vegan", veganDes),
+//            DietModel(false, imgCustom, "Custom", "Custom Diet")
+//
+//        )
+//
+//        val recyclerView: RecyclerView = view.findViewById(R.id.diet_recycler_view)
+//        val adapter = DietAdapter(dietList, DietAdapter.OnClickListener {
+//            Toast.makeText(context?.applicationContext, "Display this", Toast.LENGTH_LONG).show()
+//
+//            this.findNavController().navigate(R.id.action_navigation_diet_to_navigation_classifyproducts)
+//        })
+//        recyclerView.adapter = adapter
+//
+//        return view
+//    }
+//
+//}
