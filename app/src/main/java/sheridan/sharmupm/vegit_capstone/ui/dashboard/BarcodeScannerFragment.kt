5e3da0fc434e4c2d0eba327fb4c4e318b12e5aa0
fanxing1017 <fanxing1017@gmail.com>
2021-12-09 20:04:19 +0800

    Fix grocery list - clean name before adding & delete Fix classifyProduct - image rotation in imageView Camera will now capture the barcode and then transmit to barcode reader fragment instead of recognizing barcode automatically.

--- app/src/main/java/sheridan/sharmupm/vegit_capstone/ui/dashboard/BarcodeScannerFragment.kt
+++ app/src/main/java/sheridan/sharmupm/vegit_capstone/ui/dashboard/BarcodeScannerFragment.kt
@@ -5,13 +5,22 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.util.SparseArray
 import android.view.*
 import android.widget.*
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.util.isNotEmpty
@@ -27,6 +36,7 @@ import org.json.JSONObject
 import sheridan.sharmupm.vegit_capstone.R
 import java.io.FileNotFoundException
 import java.io.IOException
+import java.nio.ByteBuffer
 
 
 class BarcodeScannerFragment : Fragment() {
@@ -36,6 +46,8 @@ class BarcodeScannerFragment : Fragment() {
     private val client = OkHttpClient()
     private lateinit var bottomSheetDialog: BottomSheetDialog
     private lateinit var bottomSheetView:View
+    private lateinit var viewFinder: PreviewView
+    private var imageCapture: ImageCapture?=null
 
     private var completed = false
 
@@ -56,25 +68,30 @@ class BarcodeScannerFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_barcode_scanner, container, false)
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.clear()
+
+
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val galleryBtn = view.findViewById<ImageButton>(R.id.gallery_btn)
-        surfaceView = view.findViewById(R.id.camera_preview)
+        val takePhotoBtn = view.findViewById<ImageButton>(R.id.btnTakePhoto)
+        viewFinder = view.findViewById<PreviewView>(R.id.viewFinder)
 
-        completed = false
-
-
-
-
-        surfaceView.visibility = View.GONE
-        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
-            askForCameraPermission()
-            surfaceView.visibility = View.VISIBLE
+        if (allPermissionGranted()){
+            //Toast.makeText(context?.applicationContext,"We have permission",Toast.LENGTH_SHORT).show()
+            startCamera()
         }else{
-            surfaceView.visibility = View.VISIBLE
-            setupControls()
+            requestPermissions(Constants.REQUIRED_PERMISSION,Constants.REQUEST_CODE_PERMISSION_CAMERA)
 
         }
+        takePhotoBtn.setOnClickListener {
+            takePhoto()
+        }
+
         galleryBtn.setOnClickListener {
             if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
                 if (context?.applicationContext?.let { it1 -> ContextCompat.checkSelfPermission(
@@ -101,6 +118,54 @@ class BarcodeScannerFragment : Fragment() {
     }
 
 
+
+
+    private fun takePhoto() {
+        val imageCapture = imageCapture ?: return
+        imageCapture.takePicture(ContextCompat.getMainExecutor(context), object :
+            ImageCapture.OnImageCapturedCallback() {
+            override fun onCaptureSuccess(image: ImageProxy) {
+                //get bitmap from image
+                val bitmap = imageProxyToBitmap(image)
+                val rotateBitmap = bitmap.rotate(90.0f)
+
+
+                val barcodeReaderFragment = BarcodeReaderFragment()
+
+                val bundle = Bundle()
+                bundle.putParcelable("bitmap", rotateBitmap)
+                barcodeReaderFragment.arguments = bundle
+                fragmentManager?.beginTransaction()
+                    ?.replace(R.id.nav_host_fragment, barcodeReaderFragment)?.commit()
+
+
+
+
+                super.onCaptureSuccess(image)
+            }
+
+        })
+    }
+
+    fun Bitmap.rotate(degrees: Float): Bitmap {
+        val matrix = Matrix().apply { postRotate(degrees) }
+        return Bitmap.createBitmap(this, 0, 0, width, height, matrix, true)
+    }
+
+    private fun imageProxyToBitmap(image: ImageProxy): Bitmap {
+        val planeProxy = image.planes[0]
+        val buffer: ByteBuffer = planeProxy.buffer
+        val bytes = ByteArray(buffer.remaining())
+        buffer.get(bytes)
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+    }
+
+    private fun allPermissionGranted()=
+        Constants.REQUIRED_PERMISSION.all{
+            ContextCompat.checkSelfPermission(context?.applicationContext!!,it)== PackageManager.PERMISSION_GRANTED
+        }
+
+
     private fun pickImageFromGallery(){
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
@@ -108,14 +173,7 @@ class BarcodeScannerFragment : Fragment() {
 
     }
 
-    private fun setupControls(){
-        detector = BarcodeDetector.Builder(context).build()
-        cameraSource = CameraSource.Builder(context, detector).setAutoFocusEnabled(true).build()
-        surfaceView.holder.addCallback(surgaceCallBack)
-        detector.setProcessor(processor)
 
-
-    }
 
     private fun askForCameraPermission(){
         requestPermissions(arrayOf(Manifest.permission.CAMERA), requestCodeCameraPermission)
@@ -164,187 +222,95 @@ class BarcodeScannerFragment : Fragment() {
                 }
             }
         }
-        if (requestCode == requestCodeCameraPermission && grantResults.isNotEmpty()){
-            if (grantResults[0]==PackageManager.PERMISSION_GRANTED){
-                setupControls()
+        if (requestCode == Constants.REQUEST_CODE_PERMISSION_CAMERA){
+            if (allPermissionGranted()){
+                startCamera()
+
             }else{
-                Toast.makeText(context, "Permission Denied", Toast.LENGTH_LONG).show()
+                Toast.makeText(context?.applicationContext,"Permissions not granted by the user",Toast.LENGTH_SHORT).show()
+                //finish()
+                activity?.fragmentManager?.popBackStack()
             }
         }
 
-    }
 
-    private val surgaceCallBack = object : SurfaceHolder.Callback{
-        override fun surfaceCreated(surfaceHolder: SurfaceHolder) {
+
+
+    }
+    private fun startCamera(){
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder().build().also { mPreview->
+                mPreview.setSurfaceProvider(viewFinder.surfaceProvider)
+            }
+            imageCapture = ImageCapture.Builder().build()
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
             try{
-                if (context?.let {
-                        ActivityCompat.checkSelfPermission(
-                            it,
-                            Manifest.permission.CAMERA
-                        )
-                    } != PackageManager.PERMISSION_GRANTED
-                ) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return
-                }
-                cameraSource.start(surfaceHolder)
-
-            }catch (exception: Exception){
-                Toast.makeText(context, "Something went wrong", Toast.LENGTH_LONG).show()
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this,cameraSelector,preview,imageCapture)
+            }catch(e: java.lang.Exception){
+                Log.d(Constants.TAG,"Start Camera Fail:",e)
             }
-        }
-
-        override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
-            //Toast.makeText(context,"Something went wrong",Toast.LENGTH_LONG).show()
-            if (context?.let {
-                    ActivityCompat.checkSelfPermission(
-                        it,
-                        Manifest.permission.CAMERA
-                    )
-                } != PackageManager.PERMISSION_GRANTED
-            ) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return
-            }
-            cameraSource.start(holder)
-        }
-
-        override fun surfaceDestroyed(holder: SurfaceHolder) {
-            cameraSource.stop()
-        }
+        },ContextCompat.getMainExecutor(requireContext()))
     }
 
-    private val processor = object : Detector.Processor<Barcode>{
-        override fun receiveDetections(detections: Detector.Detections<Barcode>?) {
-            if (detections!=null && detections.detectedItems.isNotEmpty()){
-                val barcodes:SparseArray<Barcode> = detections.detectedItems
-                val code = barcodes.valueAt(0)
-                val url = "https://api.upcitemdb.com/prod/trial/lookup?upc="+code.rawValue
-                //AsyncTaskHandleJson().execute(url)
-                run(url)
-                //Toast.makeText(context,code.displayValue,Toast.LENGTH_LONG).show()
-            }
-
-        }
-
-        override fun release() {
-            //bottomSheetDialog.dismiss()
-
-        }
-    }
-
-
-    fun run(url: String) {
-        val request = Request.Builder()
-            .url(url)
-            .build()
-
-
-
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                activity?.runOnUiThread {
-                    Toast.makeText(context, "Fail", Toast.LENGTH_SHORT).show()
-                }
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                if (response.isSuccessful && !completed) {
-                    activity?.runOnUiThread {
-                        // Toast.makeText(this@MainActivity, response.body()?.string(), Toast.LENGTH_SHORT).show()
-                        val jsonObject = JSONObject(response.body()?.string())
-                        //val jsonObject = JSONObject(Gson().toJson(response.body()))
-                        val itemsArr = jsonObject.getJSONArray("items")
-                        if (itemsArr.length() == 0) {
-                            return@runOnUiThread
-                        }
-                        completed = true
-                        val items = itemsArr.getJSONObject(0)
-                        val title = items.getString("title")
-                        val category = items.getString("category")
-                        val lowestPrice = items.getDouble("lowest_recorded_price")
-                        val highestPrice = items.getDouble("highest_recorded_price")
-                        val pictureArray = items.getJSONArray("images")
-                        val picture = pictureArray.getString(0)
-                        val upc = items.getString("upc")
-
-                        val sharedPref = activity!!.getSharedPreferences("myPref", Context.MODE_PRIVATE)
-                        val editor = sharedPref.edit()
-                        editor.apply{
-                            putString("title",title)
-                            putString("category",category)
-                            putString("imageUrl",picture)
-                            apply()
-                        }
-                        //Toast.makeText(context,title,Toast.LENGTH_LONG).show()
-                        bottomSheetDialog = BottomSheetDialog(
-                            requireContext(),
-                            R.style.BottomSheetDialogTheme
-                        )
-                        bottomSheetView = LayoutInflater.from(context).inflate(
-                            R.layout.barcode_dialog_layout,
-                            view?.findViewById(R.id.barcode_dialog)
-                        )
-                        bottomSheetDialog.setContentView(bottomSheetView)
-                        val txtProductName =
-                            bottomSheetView.findViewById<TextView>(R.id.product_name)
-                        val txtProductCategory =
-                            bottomSheetView.findViewById<TextView>(R.id.product_category)
-                        val txtLowestPrice =
-                            bottomSheetView.findViewById<TextView>(R.id.product_lowest_price)
-                        val txtHighestPrice =
-                            bottomSheetView.findViewById<TextView>(R.id.product_highest_price)
-                        val txtUPC = bottomSheetView.findViewById<TextView>(R.id.product_upc)
-                        val productImg =
-                            bottomSheetView.findViewById<ImageView>(R.id.product_picture)
-                        val barcodeBtn = bottomSheetView.findViewById<Button>(R.id.btn_scan_barcode)
-                        val ingredientBtn =
-                            bottomSheetView.findViewById<Button>(R.id.btn_scan_ingredient)
-                        val barcodeScannerFragment = BarcodeScannerFragment()
-                        val cameraFragment = CameraFragment()
-                        txtProductName.text = title
-                        txtProductCategory.text = category
-                        txtUPC.text = upc
-                        txtLowestPrice.text = lowestPrice.toString()
-                        txtHighestPrice.text = highestPrice.toString()
-                        Picasso.get().load(Uri.parse(picture)).into(productImg)
-                        barcodeBtn.setOnClickListener {
-                            fragmentManager?.beginTransaction()?.replace(
-                                R.id.nav_host_fragment,
-                                barcodeScannerFragment
-                            )?.commit()
-                            completed = false
-                            bottomSheetDialog.dismiss()
-                        }
-                        ingredientBtn.setOnClickListener {
-                            fragmentManager?.beginTransaction()?.replace(
-                                R.id.nav_host_fragment,
-                                cameraFragment
-                            )?.commit()
-                            completed = false
-                            bottomSheetDialog.dismiss()
-                        }
+//    private val surgaceCallBack = object : SurfaceHolder.Callback{
+//        override fun surfaceCreated(surfaceHolder: SurfaceHolder) {
+//            try{
+//                if (context?.let {
+//                        ActivityCompat.checkSelfPermission(
+//                            it,
+//                            Manifest.permission.CAMERA
+//                        )
+//                    } != PackageManager.PERMISSION_GRANTED
+//                ) {
+//                    // TODO: Consider calling
+//                    //    ActivityCompat#requestPermissions
+//                    // here to request the missing permissions, and then overriding
+//                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//                    //                                          int[] grantResults)
+//                    // to handle the case where the user grants the permission. See the documentation
+//                    // for ActivityCompat#requestPermissions for more details.
+//                    return
+//                }
+//                cameraSource.start(surfaceHolder)
+//
+//            }catch (exception: Exception){
+//                Toast.makeText(context, "Something went wrong", Toast.LENGTH_LONG).show()
+//            }
+//        }
+//
+//        override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+//            //Toast.makeText(context,"Something went wrong",Toast.LENGTH_LONG).show()
+//            if (context?.let {
+//                    ActivityCompat.checkSelfPermission(
+//                        it,
+//                        Manifest.permission.CAMERA
+//                    )
+//                } != PackageManager.PERMISSION_GRANTED
+//            ) {
+//                // TODO: Consider calling
+//                //    ActivityCompat#requestPermissions
+//                // here to request the missing permissions, and then overriding
+//                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//                //                                          int[] grantResults)
+//                // to handle the case where the user grants the permission. See the documentation
+//                // for ActivityCompat#requestPermissions for more details.
+//                return
+//            }
+//            cameraSource.start(holder)
+//        }
+//
+//        override fun surfaceDestroyed(holder: SurfaceHolder) {
+//            cameraSource.stop()
+//        }
+//    }
 
 
-                        bottomSheetDialog.show()
 
-                    }
-                }
-            }
-        })
-    }
+
+
 
 
 }
--- app/src/main/java/sheridan/sharmupm/vegit_capstone/ui/dashboard/Constants.kt
+++ app/src/main/java/sheridan/sharmupm/vegit_capstone/ui/dashboard/Constants.kt
@@ -6,5 +6,6 @@ object Constants {
     const val TAG = "cameraX"
     const val FILE_NAME_FORMAT = "yy-MM-dd-HH-mm-ss-SSS"
     const val REQUEST_CODE_PERMISSION = 123
+    const val REQUEST_CODE_PERMISSION_CAMERA = 100
     val REQUIRED_PERMISSION = arrayOf(Manifest.permission.CAMERA)
 }
